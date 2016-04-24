708b2cd34e16884a95974927ba7461e22e509f88
0x6e <0x6e@users.noreply.github.com>
2016-04-24 12:38:21 +0100

    Now using Promises

--- bb-images.js
+++ bb-images.js
@@ -4,39 +4,50 @@ const util = require('util');
 
 const apiDetails = JSON.parse(fs.readFileSync('api-details.json', 'utf8'));
 
-function getMatchData(matchId)
+function getMatchDetail(matchId)
 {
   const apiPath = '/bbdb/api/matchdetail.php?match_id=%d&api_key=%s&version=%d';
 
-  var options = {
-    hostname: apiDetails.host
-    , path: util.format(apiPath, matchId, apiDetails.key, apiDetails.version)
-  };
+  return new Promise( function(resolve, reject) {
+    var options = {
+      hostname: apiDetails.host
+      , path: util.format(apiPath, matchId, apiDetails.key, apiDetails.version)
+    };
 
-  http.get(options, (res) => {
+    http.get(options, (res) => {
 
-    res.on("data", function(chunk) {
-      var body;
-      try {
-        body = JSON.parse(chunk);
-      } catch (e) {
-        // Not all responses return JSON
-      }
+      res.on('data', (chunk) => {
+        var body;
+        try {
+          body = JSON.parse(chunk);
+        } catch (e) {
+          // Not all responses return JSON
+        }
 
-      if ( body === undefined || body.error_code !== undefined) {
-        console.log("This is not the JSON you are looking for.");
-        // Bomb out here
-        return;
-      }
+        if (body === undefined) {
+          // The one case not handled with JSON
+          reject('Invalid match ID');
+          return;
+        }
 
-      console.log(body);
+        if (body.error_code !== undefined) {
+          reject(body.error_description);
+          return;
+        }
 
+        // console.log(body);
+        resolve(body);
+      });
+
+      res.resume();
+    }).on('error', (e) => {
+      reject(e.message);
     });
-
-    res.resume();
-  }).on('error', (e) => {
-    console.log(`Got error: ${e.message}`);
   });
 }
 
-getMatchData(173);
+getMatchDetail(173).then((matchReport) => {
+  console.log(matchReport);
+}).catch ((error) => {
+  console.log(error);
+});
