00a3f7d8353c9799aa34ef87c6f6fb04729ca06e
0x6e <0x6e@users.noreply.github.com>
2016-04-27 13:55:28 +0100

    Cracked basic SVG manipulation using xmldom

---
+++ svg-test.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const xmldom = require('xmldom')
+
+var parser = new xmldom.DOMParser();
+var serializer = new xmldom.XMLSerializer();
+
+fs.readFile('test.svg', 'utf8', (error, data) => {
+    if (error)
+        throw error;
+
+    var doc = parser.parseFromString(data, 'image/svg+xml');
+
+    var element = doc.getElementById('homeTeam');
+    element.textContent = "No Stable Isotopes";
+
+    element = doc.getElementById('homeScore');
+    element.textContent = "4";
+
+    element = doc.getElementById('awayTeam');
+    element.textContent = "The Cheeseosophers"
+
+    element = doc.getElementById('awayScore');
+    element.textContent = "5";
+
+    var docSvg = serializer.serializeToString(doc);
+    fs.writeFile('output.svg', docSvg);
+});
