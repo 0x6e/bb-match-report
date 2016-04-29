297c03ad135d05c5db9eb6e1cd81e093fccf7aa4
0x6e <0x6e@users.noreply.github.com>
2016-04-29 20:57:41 +0100

    Made a start on database access with Heroku

---
+++ main.js
@@ -0,0 +1,20 @@
+var pg = require('pg');
+
+pg.connect(process.env.DATABASE_URL, function(error, client, done)
+{
+    if (error)
+        throw error;
+
+    client.query('SELECT * FROM test_table', function(error, result)
+    {
+        done();
+        if (error)
+        {
+            console.log(error);
+        }
+        else
+        {
+            console.log(result.rows);
+        }
+    });
+});
