2ce00f050bdb43e74b729f5ac3f1d3cdf515c99c
0x6e <0x6e@users.noreply.github.com>
2016-06-08 22:51:47 +0100

    Added a directive to render match report images built from a template and report

--- image-builder/imageBuilder.js
+++ image-builder/imageBuilder.js
@@ -11,19 +11,29 @@
 
     ImageBuilder.prototype.build = function(template, matchReport)
     {
+        console.log(matchReport);
         var doc = this.parser.parseFromString(template, 'image/svg+xml');
 
+        function updateInnerText(element, text)
+        {
+            var elements = element.getElementsByTagName('tspan');
+            if (elements.length === 0)
+                return;
+
+            elements[0].textContent = text;
+        }
+
         var element = doc.getElementById('homeTeam');
-        element.textContent = matchReport.homeTeam;
+        updateInnerText(element, matchReport.homeTeam);
 
         element = doc.getElementById('homeScore');
-        element.textContent = matchReport.homeScore;
+        updateInnerText(element, matchReport.homeScore);
 
         element = doc.getElementById('awayTeam');
-        element.textContent = matchReport.awayTeam;
+        updateInnerText(element, matchReport.awayTeam);
 
         element = doc.getElementById('awayScore');
-        element.textContent = matchReport.awayScore;
+        updateInnerText(element, matchReport.awayScore);
 
         return this.serializer.serializeToString(doc);
     }
@@ -37,9 +47,29 @@
     {
         // AngularJS module definition
         var imageBuilder = angular.module('imageBuilder', []);
+
+        // The ImageBuilderFactory
         imageBuilder.factory('ImageBuilder', [function ImageBuilderFactory()
         {
             return new ImageBuilder(new DOMParser(), new XMLSerializer());
+        }]);
+
+        // A directive for rendering the SVG images
+        imageBuilder.directive('matchReportSvg', [ 'ImageBuilder', function(ImageBuilder)
+        {
+            function postLink(scope, element, attributes)
+            {
+                var template = attributes.svg;
+                var report = angular.fromJson(attributes.report);
+                
+                element.html(ImageBuilder.build(template, report));
+            }
+
+            return {
+                restrict: 'E',
+                templateNamespace: 'svg',
+                link: postLink
+            };
         }]);
     }
     else if (isNode)
